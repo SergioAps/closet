2632814f562308a9190a5e9b7dbcd06e8c370879
Eduardo Aguilar <laloaps@gmail.com>
2022-10-17 16:31:53 -0400

    test product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+  validates :name, :description, :image_url, :adquisition_date, presence: true
+  validates :name, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|jpeg|png)\z}i,
+        message: 'must be a URL for GIF, JPEG, JPG or PNG image.'
+  }
+  validates :adquisition_date, presence: true, comparison: { less_than: DateTime.now }
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "product attributes must not be empty" do product = Product.new
+    assert product.invalid?
+    assert product.errors[:name].any?
+    assert product.errors[:description].any?
+    assert product.errors[:image_url].any?
+    assert product.errors[:adquisition_date].any?
+  end
 end

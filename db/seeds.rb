1708739f41284171c748dcb1e3ec6ecc4b0a8887
Eduardo Aguilar <laloaps@gmail.com>
2022-10-17 15:37:22 -0400

    created product model and product seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Product.delete_all
+Product.create!(name: 'Black tshirt',
+  description: %{black},
+  image_url: 'tshirt.jpg',
+  adquisition_date: DateTime.new(2020,9,1,17))
+
+Product.create!(name: 'White tshirt',
+  description: %{white},
+  image_url: 'white.jpeg',
+  adquisition_date: DateTime.new(2021,9,1,17))
+
+Product.create!(name: 'Colours tshirt',
+  description: %{rainbow},
+  image_url: 'colours.jpeg',
+  adquisition_date: DateTime.new(2022,9,1,17))

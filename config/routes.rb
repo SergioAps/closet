7b92066223a6830432c81130e886b1ce88a9ba41
Eduardo Aguilar <laloaps@gmail.com>
2022-10-21 12:42:55 -0400

    devise sign out completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users do
-  root to: 'home#index'
+  root to: 'products#index'
+  devise_for :users
   resources :products
+
 
   # Defines the root path route ("/")
   # root "articles#index"
